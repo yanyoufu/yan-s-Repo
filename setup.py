d7ae9e70faea19b544729c2df481ff84fe7c65cb
yanyoufu <yanyoufu1988@qq.com>
2018-03-08 21:21:35 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hello_flask',
+	version='1.0',
+	description='OpenShift V3 Flask App',
+	author='y',
+	author_email='y@y.com',
+	url='http://www.python.org/sigs/distutils-sig/',
+	install_requires=['Flask>=0.10.1'],
+	)
+	
