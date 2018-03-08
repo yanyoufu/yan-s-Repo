b0293919636ec601ae705bfb27c2ed773a9fb51e
yanyoufu <yanyoufu1988@qq.com>
2018-03-08 22:52:08 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+import os
+
+from helloflask import app as application
+
+if __name__ == '__main__':
+	from wsgiref.simple_server import make_server
+	httpd = make_server('localhost', 8051, application)
+	httpd.handle_request()
+	
